a39d650cdea690a994b9d46acafdb72fd661a6d9
AilysGeister <samantha.guivret@gmail.com>
2026-09-23 16:59:51 +0200

    Structure de la forme de coplien

---
+++ src/Server.cpp
@@ -0,0 +1,43 @@
+class Server {
+    public:
+        Server();
+        Server(const Server&);
+        ~Server();
+        Server& operator= (const Server&);
+        void consoleWriter();
+        void fileWriter();
+};
+
+/*
+Constructeurs & Destructeur
+*/
+Server::Server() {
+
+}
+
+Server::Server(const Server&) {
+
+}
+
+Server::~Server() {
+
+}
+
+/*
+Opérateurs
+*/
+
+Server& Server::operator= {
+
+}
+
+/*
+Méthodes
+*/
+Server::consoleWriter() {
+
+}
+
+Server::fileWriter() {
+    
+}
