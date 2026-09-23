b8bff630f9587d3adf2939633a3a30a9a3935065
Lofane <165309408+Ghost0mega@users.noreply.github.com>
2026-09-23 17:46:18 +0200

    Untrack specific files

--- build/CMakeFiles/forcebuild.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/forcebuild"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/forcebuild.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
